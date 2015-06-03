94ab9ab8b6e308c000ad53abfea0330754408dd7
eshuvaeva <Elizshuvaeva@yandex.ru>
2015-06-03 23:17:35 +0300

    Smoothing functions improved

--- knots.py
+++ knots.py
@@ -3,51 +3,38 @@ class Link:
 		self.n = n
 	def inf(self,ar):
 		self.inf = ar
+	def loop(self,vertex):
+		loop_edges = []
+		for i in range(len(self.inf[vertex-1])):
+			if self.inf[vertex-1][i] == vertex:
+				loop_edges.append[i]
+		return(loop_edges)
 	def zero_smoothing(self,vertex):
-#find which vertex v1 is the 1st in v.neighbors(right up)
-#find which vertex is the 3rd in v1.neighbors(left down)
-#change 3rd v1.neighbors(left down) into 2nd in v.neighbours(left up)
-		self.inf[self.inf[vertex-1][1]-1][3]= self.inf[vertex-1][2]
-#find which vertex v2 is the 2nd in v.neighbors(left up)
-#find which vertex is the 4th in v2.neighbors(right down)
-#change 4th v2.neighbors(right down) into 1st in v.neighbours(right up)
-		self.inf[self.inf[vertex-1][2]-1][4]= self.inf[vertex-1][1]
-#find which vertex v3 is the 3rd in v.neighbors(left down)
-#find which vertex is the 1st in v3.neighbors(right up)
-#change 1st v3.neighbors(right up) into 4th in v.neighbours(right down)
-		self.inf[self.inf[vertex-1][3]-1][1]= self.inf[vertex-1][4]
-#find which vertex v4 is the 4th in v.neighbors(right down)
-#find which vertex is the 2nd in v4.neighbors(left up)
-#change 2nd v4.neighbors(left up) into 3rd in v.neighbours(left down)
-		self.inf[self.inf[vertex-1][4]-1][2]= self.inf[vertex-1][3]
+		le =  self.loop(vertex)
+		if len(le) == 0:
+			self.inf[self.inf[vertex-1][1]-1][3]= self.inf[vertex-1][2]
+			self.inf[self.inf[vertex-1][2]-1][4]= self.inf[vertex-1][1]
+			self.inf[self.inf[vertex-1][3]-1][1]= self.inf[vertex-1][4]
+			self.inf[self.inf[vertex-1][4]-1][2]= self.inf[vertex-1][3]
+		# if len(le) == 2:
+		# 	if le == [1,4]:
+				
+		if len(le) == 4:
+			self.inf[vertex-1] = []
+
 	def one_smoothing(self,vertex):
-#find which vertex v1 is the 1st in v.neighbors(right up)
-#find which vertex is the 3rd in v1.neighbors(left down)
-#change 3rd v1.neighbors(left down) into 4th in v.neighbours(right down)
 		self.inf[self.inf[vertex-1][1]-1][3]= self.inf[vertex-1][4]
-#find which vertex v2 is the 2nd in v.neighbors(left up)
-#find which vertex is the 4th in v2.neighbors(right down)
-#change 4th v2.neighbors(right down) into 1st in v.neighbours(right up)
 		self.inf[self.inf[vertex-1][2]-1][4]= self.inf[vertex-1][3]
-#find which vertex v3 is the 3rd in v.neighbors(left down)
-#find which vertex is the 1st in v3.neighbors(right up)
-#change 1st v3.neighbors(right up) into 2nd in v.neighbours(left up)
 		self.inf[self.inf[vertex-1][3]-1][1]= self.inf[vertex-1][2]
-#find which vertex v4 is the 4th in v.neighbors(right down)
-#find which vertex is the 2nd in v4.neighbors(left up)
-#change 2nd v4.neighbors(left up) into 1st in v.neighbours(right up)
 		self.inf[self.inf[vertex-1][4]-1][2]= self.inf[vertex-1][1]
 class Vertex:
-	def __init__(self, type):
-		self.type = type
+	def __init__(self, n):
+		self.num = n
 #right up;left up; left down ; right down
 	def neighbors(self,list):
 		self.neighbors = list
-class Polynomial:
-	def __init__(self,link):
-		self.link = link
 
-		
+
 num = ["1","2","3","4","5","6","7","8","9","0"]
 n = int(input())
 m = int(input())
