852188bd68e7b9730a2ba3f20127bf1eff163047
eshuvaeva <Elizshuvaeva@yandex.ru>
2015-06-06 23:10:52 +0300

    Input changed and improved

--- input.py
+++ input.py
@@ -1,40 +1,32 @@
-class Link:
-	def __init__(self, n):
-		self.n = n
-	def inf(self,ar):
-		self.inf = ar
-	def loop(self,vertex):
-		loop_edges = []
-		for i in range(len(self.inf[vertex-1])):
-			if self.inf[vertex-1][i] == vertex:
-				loop_edges.append[i]
-		return(loop_edges)
-
-class Vertex:
-	def __init__(self, n):
-		self.num = n
-#right up;left up; left down ; right down
-	def neighbors(self,list):
-		self.neighbors = list
-
 def is_int(str):
 	try:
 		int(str)
 		return True
 	except ValueError:
 		return False
+class Link:
+	def __init__(self, n):
+		self.n = n
+	def inf(self,ar):
+		self.inf = ar
 
-n = int(input())# number of disconnected components
+
+n = int(input())# число несвязанных между собой компонент
 knot = Link(n)
 knot.inf = []
-for j in range(n):#separate input for each component
-	m=int(input())#number of vertices in each component
+for j in range(n):#отдельно вводим данные для каждой компоненты
+	m=int(input())#для каждой компоненты вводим число вершин в ней
 	ar0 =[]
 	for i in range(m):
-		ar = list(map(str,input().split()))
-		for j in range(len(ar)):
-			if is_int(ar[j]) == True:#numbers of neighbor vertices converted to integer
-				ar[j] = int(ar[j])
-		ar0.append(ar)
-	knot.inf.append(ar0) 
+		vertex_inf = []
+		for j in range(5):
+			edge = list(map(str,input().split()))
+			for t in range(len(edge)):
+				if is_int(edge[t]) == True:#переводим номера вершин-соседей в integer
+					edge[t] = int(edge[t])
+			vertex_inf.append(edge)
+		ar0.append(vertex_inf)
+	knot.inf.append(ar0)
+print(knot.inf)
+
 
