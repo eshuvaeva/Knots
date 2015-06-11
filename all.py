df18572b2aafc5968ab3458bb9d7a01b3ee16105
eshuvaeva <Elizshuvaeva@yandex.ru>
2015-06-11 22:18:30 +0300

    smoothing functions slightly improved

--- all.py
+++ all.py
@@ -90,7 +90,10 @@ class Link:
 		if len(le) == 4:
 			if check_loops(comp[vertex-1]) == True:
 				self.inf.append([])
+			if len(comp)>1:
+				self.inf.append([])
 		del(comp[vertex-1])
+		return self
 	def one_smoothing(self,comp,vertex):
 		le = self.loop(comp,vertex)
 		if len(le) == 0:
@@ -140,7 +143,10 @@ class Link:
 		if len(le) == 4:
 			if check_loops(comp[vertex-1]) == False:
 				self.inf.append([])
+			if len(comp)>1:
+				self.inf.append([])
 		del(comp[vertex-1])
+		return self
 
 class LaurentPolynomial():
 	def __init__(self,min_power,coeff):
